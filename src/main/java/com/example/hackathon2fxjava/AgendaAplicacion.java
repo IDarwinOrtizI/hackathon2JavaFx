2781230a14a26bca394142e3c1e0904dc34d93e0
Samary Cardozo <samarycardozo@gmail.com>
2025-12-18 16:35:11 -0500

    subir style css agenda

--- src/main/java/com/example/hackathon2fxjava/AgendaAplicacion.java
+++ src/main/java/com/example/hackathon2fxjava/AgendaAplicacion.java
@@ -8,11 +8,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AgendaAplicacion extends Application {
-    @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AgendaAplicacion.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+        //cargar css
+        var cssResource = AgendaAplicacion.class.getResource("style.css");
+        if (cssResource != null) {
+            scene.getStylesheets().add(cssResource.toExternalForm());
+        } else {
+            System.out.println("Error: No se encontró el archivo style.css en la ruta especificada.");
+        }
+
+        stage.setTitle("Agenda Pro");
         stage.setScene(scene);
         stage.show();
     }
